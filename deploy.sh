2a2f42fb5bfdf1e44702ef460c3a6ad56eef6bc9
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2017-12-31 18:11:22 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag ${APP_IMAGE} quay.io/${APP_IMAGE}:${TRAVIS_TAG}
-docker tag ${APP_IMAGE} quay.io/${APP_IMAGE}:latest
-docker images
-docker push quay.io/${APP_IMAGE}:${TRAVIS_TAG}
-docker push quay.io/${APP_IMAGE}:latest
-
 # Obtain the application repository and log in
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm  \
