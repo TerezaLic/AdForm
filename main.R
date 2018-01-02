ba29b725d362d8856d601e25d65bf257f0656645
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-01-03 00:00:46 +0100

    Update main.R

--- main.R
+++ main.R
@@ -14,43 +14,56 @@ app$readConfig()
 
 # access the supplied value of 'myParameter'
 apiKey<-app$getParameters()$`#apiKey`
-dateFrom<-as.Date(app$getParameters()$dateFrom)
-dateTo<-as.Date(app$getParameters()$dateTo)
+dateFrom<-app$getParameters()$dateFrom
+dateTo<-app$getParameters()$dateTo
 dataProviderId<-app$getParameters()$dataProviderId
-endpoint<-app$getParameters()$report
+ContentType<-"text/csv"
 
 # read user input data from JSON config editor ----------TO BE UPDATED WITH new JSON form------------
 # user<-app$getParameters()$user
 # Password<-app$getParameters()$Password
-# dataProviderId<-app$getParameters()$dataProviderId
-# dateFrom<-app$getParameters()$dateFrom
-# dateTo<-app$getParameters()$dateTo
 # out_bucket<-app$getParameters()$bucket
 
 ##Catch config errors
 
 if(is.null(apiKey)) stop("enter your apiKey in the user config field")
 
-# get csv file name with full path from output mapping
-outName <- app$getExpectedOutputTables()['full_path']
-
 
 #=======Actual API call========#
 
-# report Datausage je automaticky group by Agency
-# report Audience vyzaduje Provider Id
+# create IN tables
 
 url <- "https://api.adform.com/"
-req <- httr::GET(url,path=endpoint,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Authorization = apiKey))
-json <- httr::content(req, as = "text")
-Datausage<-fromJSON(json)
 
 
-# write output data
-write.csv(Datausage,"tests/data/out/tables/result.csv",row.names = FALSE)
+endpoint1<-"v2/dmp/reports/datausage"
+req1 <- httr::GET(url,path=endpoint1,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+datausage<-httr::content(req1, as="parse")
+write.csv(datausage,"tests/data/in/tables/datausage.csv",row.names = FALSE)
 
-# write table metadata
+endpoint2<-"v2/dmp/reports/audience"
+req2 <- httr::GET(url,path=endpoint2,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+audience<-httr::content(req2, as="parse")
+write.csv(audience,"tests/data/in/tables/audience.csv",row.names = FALSE)
 
+endpoint3<-"/v2/dmp/dataproviders/{dataProviderId}/audience"
+req3 <- httr::GET(url,path=endpoint3,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+DataProvidersAudience<-httr::content(req3, as="parse")
+write.csv(DataProvidersAudience,"tests/data/in/tables/DataProvidersAudience.csv",row.names = FALSE)
 
+endpoint4<-"v2/dmp/reports/billing/overall"
+req4 <- httr::GET(url,path=endpoint4,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+billing<-httr::content(req4, as="parse")
+write.csv(billing,"tests/data/in/tables/billing.csv",row.names = FALSE)
 
+remove(req1,req2,req3, req4)
+remove(ContentType,endpoint1,endpoint2,endpoint3,endpoint4)
 
+# transformation
+datausageTOTAL<-data.frame(dateFrom,dateTo,billing$DataProvider,sum(datausage$Revenue),sum(datausage$RevenueInAdvertiserCurrency),sum(datausage$`Adform Gross Revenue`),sum(datausage$DataProviderRevenue), sum(datausage$DataProviderRevenueInAdvertiserCurrency),sum(datausage$`Owner Net Revenue (EUR)`),sum(datausage$AdformRevenue),sum(datausage$`Adform Net Revenue(EUR)`),sum(datausage$AdformRevenueInAdvertiserCurrency))
+
+# create OUT tables
+write.csv(billing,"tests/data/out/tables/billing.csv",row.names = FALSE)
+write.csv(datausageTOTAL,"tests/data/out/tables/datausage.csv",row.names = FALSE)
+write.csv(audience,"tests/data/out/tables/audience.csv",row.names = FALSE)
+write.csv(DataProvidersAudience,"tests/data/in/tables/DataProvidersAudience.csv",row.names = FALSE)
