812d062e374267b2e313b342b9b8d1bb34e05f7e
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-01-04 09:37:26 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,8 +1,8 @@
 
-
 #=======Install the libraries=========#
 library(lubridate)
 library(httr)
+library(jsonlite)
 library('keboola.r.docker.application')
 
 #=======CONFIGURATION========#
@@ -13,58 +13,63 @@ app$readConfig()
 
 
 # access the supplied value of 'myParameter'
-apiKey<-app$getParameters()$`#apiKey`
 dateFrom<-app$getParameters()$dateFrom
 dateTo<-app$getParameters()$dateTo
-dataProviderId<-app$getParameters()$dataProviderId
 grouping<-app$getParameters()$groupBy
 ContentType<-"text/csv"
-
-# read user input data from JSON config editor ----------TO BE UPDATED WITH new JSON form------------
-# user<-app$getParameters()$user
-# Password<-app$getParameters()$Password
-# out_bucket<-app$getParameters()$bucket
-
-##Catch config errors
-
-if(is.null(apiKey)) stop("enter your apiKey in the user config field")
+user<-app$getParameters()$user
+password<-app$getParameters()$`#pass`
 
 
 #=======Actual API call========#
 
-# create IN tables
+# get session token
 
 url <- "https://api.adform.com/"
+req <- httr::POST(url,path="v1/dmp/token",body=list(grant_type="password",username="",password=""),encode = "form")
+json <- httr::content(req, as = "text")
+returnData<-fromJSON(json)
+apiKey <- paste("Bearer ",returnData$access_token)
 
+# catch login errors
+if (req$status_code != 200) stop("unauthorized - verify username & password")
+
+# get list of data providers
+endpoint0<-"/v1/dmp/dataproviders"
+req0 <-httr::GET(url,path=endpoint0,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+dataProviders<-httr::content(req0, as="parse")
+dataProvidersId<-list(dataProviders$Id)
+
+
+
+
+# create IN tables
 
 endpoint1<-"v2/dmp/reports/datausage"
-req1 <- httr::GET(url,path=endpoint1,query=list(groupBy=grouping,dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+req1 <- httr::GET(url,path=endpoint1,query=list(groupBy=grouping,dataProviderId=dataProvidersId$Id ,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
 datausage<-httr::content(req1, as="parse")
-write.csv(datausage,"tests/data/in/tables/datausage.csv",row.names = FALSE)
+write.csv(datausage,"out/tables/datausage.csv",row.names = FALSE)
+
+remove(req1,endpoint1,datausage)
 
 endpoint2<-"v2/dmp/reports/audience"
 req2 <- httr::GET(url,path=endpoint2,query=list(groupBy=grouping,dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
 audience<-httr::content(req2, as="parse")
-write.csv(audience,"tests/data/in/tables/audience.csv",row.names = FALSE)
+write.csv(audience,"out/tables/audience.csv",row.names = FALSE)
+
+remove(req2,endpoint2,audience)
 
 endpoint3<-"/v2/dmp/dataproviders/{dataProviderId}/audience"
 req3 <- httr::GET(url,path=endpoint3,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
 DataProvidersAudience<-httr::content(req3, as="parse")
-write.csv(DataProvidersAudience,"tests/data/in/tables/DataProvidersAudience.csv",row.names = FALSE)
+write.csv(DataProvidersAudience,"out/tables/DataProvidersAudience.csv",row.names = FALSE)
+
+remove(req3,endpoint3,DataProvidersAudience)
 
 endpoint4<-"v2/dmp/reports/billing/overall"
 req4 <- httr::GET(url,path=endpoint4,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
 billing<-httr::content(req4, as="parse")
-write.csv(billing,"tests/data/in/tables/billing.csv",row.names = FALSE)
+write.csv(billing,"out/tables/billing.csv",row.names = FALSE)
 
-remove(req1,req2,req3, req4)
-remove(ContentType,endpoint1,endpoint2,endpoint3,endpoint4)
+remove(req4,endpoint4,billing)
 
-# transformation
-datausageTOTAL<-data.frame(dateFrom,dateTo,billing$DataProvider,sum(datausage$Revenue),sum(datausage$RevenueInAdvertiserCurrency),sum(datausage$`Adform Gross Revenue`),sum(datausage$DataProviderRevenue), sum(datausage$DataProviderRevenueInAdvertiserCurrency),sum(datausage$`Owner Net Revenue (EUR)`),sum(datausage$AdformRevenue),sum(datausage$`Adform Net Revenue(EUR)`),sum(datausage$AdformRevenueInAdvertiserCurrency))
-
-# create OUT tables
-write.csv(billing,"tests/data/out/tables/billing.csv",row.names = FALSE)
-write.csv(datausageTOTAL,"tests/data/out/tables/datausage.csv",row.names = FALSE)
-write.csv(audience,"tests/data/out/tables/audience.csv",row.names = FALSE)
-write.csv(DataProvidersAudience,"tests/data/in/tables/DataProvidersAudience.csv",row.names = FALSE)
