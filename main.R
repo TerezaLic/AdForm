241f7d39cc8159f70e7be9521973e71f4506379d
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-01-05 01:49:24 +0100

    Update main.R

--- main.R
+++ main.R
@@ -4,6 +4,7 @@ library(lubridate)
 library(httr)
 library(jsonlite)
 library('keboola.r.docker.application')
+library(purrr)
 
 #=======CONFIGURATION========#
 
@@ -18,8 +19,7 @@ dateTo<-app$getParameters()$dateTo
 grouping<-app$getParameters()$groupBy
 ContentType<-"text/csv"
 user<-app$getParameters()$user
-password<-app$getParameters()$`#pass`
-dataProviderId<-"11392"
+password<-app$getParameters()$'#pass'
 
 
 #=======Actual API call========#
@@ -35,42 +35,30 @@ apiKey <- paste("Bearer ",returnData$access_token)
 # catch login errors
 if (req$status_code != 200) stop("unauthorized - verify username & password")
 
+
 # get list of data providers
-endpoint0<-"/v1/dmp/dataproviders"
-req0 <-httr::GET(url,path=endpoint0,httr::add_headers(Accept = ContentType,Authorization = apiKey))
-dataProviders<-httr::content(req0, as="parse")
-dataProvidersId<-list(dataProviders$Id)
+pIdata<-GET(url,path=endpoint0,add_headers(Authorization = apiKey))%>%content("text", encoding = "UTF-8")%>%fromJSON(flatten=TRUE,simplifyDataFrame = TRUE)
+pid<-pIdata[["id"]]
 
 
+# define API function
+get_report<-function(endpoint){
+   req<- httr::GET(url,path=endpoint,query=list(groupBy=grouping,dataProviderId=pid,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+   datasource<-httr::content(req, as="parse")
+   fname=basename(endpoint)
+   csvFileName<-paste("out/tables/",fname,"_",pid,".csv",sep = "")
+   write.csv(datasource,file=csvFileName,row.names = FALSE)
+}
 
+# data load
+endpoint1<-("v2/dmp/reports/datausage")
+get_report(endpoint1)
 
-# create IN tables
+endpoint2<-("v2/dmp/reports/audience")
+get_report(endpoint2)
 
-endpoint1<-"v2/dmp/reports/datausage"
-req1 <- httr::GET(url,path=endpoint1,query=list(groupBy=grouping,dataProviderId=dataProvidersId$Id ,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
-datausage<-httr::content(req1, as="parse")
-write.csv(datausage,"out/tables/datausage.csv",row.names = FALSE)
+endpoint3<-("/v2/dmp/dataproviders/{dataProviderId}/audience")
+get_report(endpoint3)
 
-remove(req1,endpoint1,datausage)
-
-endpoint2<-"v2/dmp/reports/audience"
-req2 <- httr::GET(url,path=endpoint2,query=list(groupBy=grouping,dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
-audience<-httr::content(req2, as="parse")
-write.csv(audience,"out/tables/audience.csv",row.names = FALSE)
-
-remove(req2,endpoint2,audience)
-
-endpoint3<-"/v2/dmp/dataproviders/{dataProviderId}/audience"
-req3 <- httr::GET(url,path=endpoint3,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
-DataProvidersAudience<-httr::content(req3, as="parse")
-write.csv(DataProvidersAudience,"out/tables/DataProvidersAudience.csv",row.names = FALSE)
-
-remove(req3,endpoint3,DataProvidersAudience)
-
-endpoint4<-"v2/dmp/reports/billing/overall"
-req4 <- httr::GET(url,path=endpoint4,query=list(dataProviderId=dataProviderId,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
-billing<-httr::content(req4, as="parse")
-write.csv(billing,"out/tables/billing.csv",row.names = FALSE)
-
-remove(req4,endpoint4,billing)
-
+endpoint4<-("v2/dmp/reports/billing/overall")
+get_report(endpoint4)
