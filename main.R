d2a43534dd538a9b13989bfcce6d4654800f7c51
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-02-24 20:18:59 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,12 +1,17 @@
+#result <- input_data
+#write.csv(result, file = "out/tables/output.csv", row.names = FALSE)
+
 
 #=======Install the libraries=========#
-library(lubridate,quietly = TRUE)
-library(httr, quietly = TRUE)
-library(jsonlite, quietly = TRUE)
-library('keboola.r.docker.application')
-library('keboola.sapi.r.client', quietly = TRUE)
-library(purrr, quietly = TRUE)
-library(foreach, quietly = TRUE)
+library(lubridate, warn.conflicts=FALSE, quietly=TRUE)
+library(httr, warn.conflicts=FALSE, quietly=TRUE)
+library(jsonlite, warn.conflicts=FALSE, quietly=TRUE)
+library('keboola.r.docker.application', warn.conflicts=FALSE, quietly=TRUE)
+#library('keboola.sapi.r.client', quietly = TRUE)
+library(purrr, warn.conflicts=FALSE, quietly=TRUE)
+library(foreach, warn.conflicts=FALSE, quietly=TRUE)
+library(stringr, warn.conflicts = FALSE, quietly = TRUE)
+
 
 #=======CONFIGURATION========#
 
@@ -14,16 +19,18 @@ library(foreach, quietly = TRUE)
 app <- keboola.r.docker.application::DockerApplication$new('/data/')
 app$readConfig()
 
-
 # access the supplied value of 'myParameter'
 dateFrom<-app$getParameters()$dateFrom
 dateTo<-app$getParameters()$dateTo
-grouping<-app$getParameters()$groupBy
-ContentType<-"text/csv"
 user<-app$getParameters()$user
 password<-app$getParameters()$'#password'
-#outDestination <- app$getParameters()$bucket
+id<-app$getParameters()$id
+textstr<-app$getParameters()'$text'
+#3 options for filter: "category", "data consumers", "segment/audience"
+filterUI<-app$getParameters()$filter
 
+ContentType<-"text/csv"
+#outDestination <- app$getParameters()$bucket
 
 #=======Actual API call========#
 
@@ -39,65 +46,216 @@ apiKey <- paste("Bearer ",returnData$access_token)
 if (req$status_code != 200) stop("unauthorized - verify username & password")
 
 
-# get list of data providers
-pIdata<-GET(url,path="/v1/dmp/dataproviders",add_headers(Authorization = apiKey))%>%content("text", encoding = "UTF-8")%>%fromJSON(flatten=TRUE,simplifyDataFrame = TRUE)
-pid<-pIdata[["id"]]
+#=======DEFINE FUNCTIONS========#
 
-# DEFINE FUNCTIONS
+# funciton to clean nested Json / df with list col
+flatten<-function (l) {
+  if (!is.list(l)) return(l)
+  do.call('rbind', lapply(l, function(x) `length<-`(x, max(lengths(l)))))
+}
+# ?? různá délka outputu problém pro KBC
+                
+# get list of Ids
+get_Id_list<-function(file){
+  read.csv(file,sep = ",")[ ,1]
+}                          
 
-# define API function w/o parameters
+# get list of Ids+ Names (needed for UI filtering)
+get_SID_list<-function(file){
+  read.csv(file,sep = ",")[ ,c(1, 8)]
+}            
+
+# define API function w/o parameters 
 get_report<-function(endpoint){
-    req<- httr::GET(url,path=endpoint,query=list(from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
-    datasource<-httr::content(req, as="parse")
-    fname=basename(endpoint)
-    csvFileName<-paste("/data/out/tables/",fname,".csv",sep = "")
-    write.csv(datasource,file=csvFileName,row.names = FALSE)
-    # write table metadata - set new primary key
-    # app$writeTableManifest(csvFileName,destination='' ,primaryKey =c('Date'))
+  req<- httr::GET(url,path=endpoint,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+  datasource<-httr::content(req, as="parse")
+  fname=basename(endpoint)
+  csvFileName<-paste("out/tables/",fname,".csv",sep = "")
+  write.csv(datasource,file=csvFileName,row.names = FALSE)
+  # write table metadata - set new primary key
+  # app$writeTableManifest(csvFileName,destination='' ,primaryKey =c('Date'))
 }
 
 # define API function with ProviderId parameter
-get_report_pId<-function(pid,endpoint){
+get_report_pId<-function(pid,endpoint,filterType){
   datasource<-foreach(i=pid,.combine='rbind',.multicombine = TRUE)%dopar%{
-    req<-httr::GET(url,path=endpoint,query=list(dataProviderId=i,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+    req<-httr::GET(url,path=endpoint,query=list(dataProviderId=i) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+    if(filterType==filterUI & !(id=="(All)") & textstr=="(All)"){
+      datasource<-httr::content(req, as="parse")%>%filter(Id == id)
+    }  else if (filterType==filterUI & id=="(All)" & !(textstr=="(All)")){
+      datasource<-httr::content(req, as="parse")%>%filter(str_detect(Name, fixed(textstr,ignore_case=TRUE)))
+    } else if(filterType==filterUI & !(id=="(All)") & !(textstr=="(All)")){
+      datasource<-httr::content(req, as="parse")%>%filter(str_detect(Name, fixed(textstr,ignore_case=TRUE)))%>%filter(Id == id)
+    } else
     datasource<-httr::content(req, as="parse")
   }
   fname=basename(endpoint)
-  csvFileName<-paste("/data/out/tables/",fname,"_by_PID",".csv",sep = "")
+  csvFileName<-paste("out/tables/",fname,"_by_PID",".csv",sep = "")
   write.csv(datasource,file=csvFileName,row.names = FALSE)
 }
 
-# define API function with ProviderId & Group by parameters
-get_report_pIdGb<-function(pid,endpoint){
-  datasource<-foreach(i=pid,.combine='rbind',.multicombine = TRUE)%dopar%{
-    req<-httr::GET(url,path=endpoint,query=list(groupBy=grouping,dataProviderId=i,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
-    datasource<-httr::content(req, as="parse")
-  }
-  fname=basename(endpoint)
-  csvFileName<-paste("/data/out/tables/",fname,"_",grouping,".csv",sep = "")
-  write.csv(datasource,file=csvFileName,row.names = FALSE)
-}
 
-# define API function with ProviderId & Group by parameters
-get_report_pIdGb2<-function(pid,endpoint){
+# define API function for report Audience
+get_report_audience<-function(pid,endpoint){
   datasource<-foreach(i=pid,.combine='rbind',.multicombine = TRUE)%dopar%{
-    req<-httr::GET(url,path=endpoint,query=list(groupBy=grouping,dataProviderId=i,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+    req<-httr::GET(url,path=endpoint,query=list(groupBy="date",dataProviderId=i,from=dateFrom,to=dateTo) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
     datasource<-httr::content(req, as="parse")
     df<-as.data.frame(datasource)
-    df$byProviderId<-i
+      if (filterUI=="category"){
+          sid<-get_Id_list("out/tables/segments_by_CID.csv")
+          df<-subset(df, `Audience ID` %in% sid)
+          }
+      else if (filterUI=="segment/audience" & !(id=="(All)") & textstr=="(All)"){
+          sid<-id
+          df<-subset(df, `Audience ID` %in% sid)
+          }
+      else if (filterUI=="segment/audience" & !(id=="(All)") & !(textstr=="(All)")){
+          sid<-id
+          df<-subset(df, `Audience ID` %in% sid)
+          }
+      else if (filterUI=="segment/audience" & id=="(All)" & !(textstr=="(All)")) {
+          sid<-get_SID_list("out/tables/segments_by_PID.csv")%>%filter(str_detect(Audience.Name, fixed(textstr,ignore_case=TRUE)))%>%select(1)
+          sid<-as.numeric(as.character(sid$Audience.ID)) 
+          df<-subset(df, `Audience ID` %in% sid)
+          }
+      else {df}
+      
+    df$byProviderId<-i 
   }
   fname=basename(endpoint)
-  csvFileName<-paste("/data/out/tables/",fname,"_",grouping,".csv",sep = "")
+  csvFileName<-paste("out/tables/",fname,"_by_date.csv",sep = "")
   write.csv(df,file=csvFileName,row.names = FALSE)
 }
 
-# data load
-get_report(endpoint="v2/dmp/reports/billing/overall")
-get_report(endpoint="v1/dmp/segments")
-get_report_pId(endpoint="/v2/dmp/dataproviders/{dataProviderId}/audience",pid)
-get_report_pId(endpoint="/v1/dmp/dataproviders/{dataProviderId}/segments",pid)
-get_report_pIdGb(endpoint="v2/dmp/reports/datausage",pid)
-get_report_pIdGb2(endpoint="v2/dmp/reports/audience",pid)
+# define API function with category ID parameter
+get_report_cId<-function(cid,endpoint){
+  datasource<-foreach(i=cid,.combine='rbind',.multicombine = TRUE)%dopar%{
+    req<-httr::GET(url,path=endpoint,query=list(categoryId=i) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+    datasource<-httr::content(req, as="parse")
+  }
+  fname=basename(endpoint)
+  csvFileName<-paste("out/tables/",fname,"_by_CID",".csv",sep = "")
+  write.csv(datasource,file=csvFileName,row.names = FALSE)
+}
+
+
+
+get_report_dcId<-function(dcid,endpoint){
+  datasource<-foreach(i=dcid,.combine='rbind',.multicombine = TRUE)%dopar%{
+    req<-httr::GET(url,path=endpoint,query=list(dataConsumerId=i) ,httr::add_headers(Accept = ContentType,Authorization = apiKey))
+    datasource<-httr::content(req, as="parse")
+  }
+  fname=basename(endpoint)
+  csvFileName<-paste("out/tables/",fname,"_by_DCID",".csv",sep = "")
+  write.csv(datasource,file=csvFileName,row.names = FALSE)
+}
+
+# define API function with segment ID parameter / JSON
+get_report_SId<-function(sid,endpoint){
+  
+    if(filterUI=="category"){
+      sid<-get_Id_list("out/tables/segments_by_CID.csv")
+          } 
+    else if (filterUI=="segment/audience" & !(id=="(All)") & textstr=="(All)"){
+            sid<-id
+    } 
+    else if (filterUI=="segment/audience" & !(id=="(All)") & !(textstr=="(All)")){
+            sid<-id
+    } 
+    else if (filterUI=="segment/audience" & id=="(All)" & !(textstr=="(All)")){
+            sid<-get_SID_list("out/tables/segments_by_PID.csv")%>%filter(str_detect(Audience.Name, fixed(textstr,ignore_case=TRUE)))%>%select(1)
+            sid<-as.numeric(as.character(sid$Audience.ID)) 
+          }
+    else {
+    # if filterUI is not "category"or "segment/audience" then assign NULL to prevent performance issue (cycling 500 possibilites one by one)
+          sid<-'no selection'
+          }
+    
+  
+  datasource<-foreach(i=sid,.combine='rbind',.multicombine = TRUE)%dopar%{
+    req<-httr::GET(url,path=endpoint,query=list(segmentId=i) ,httr::add_headers(Accept = 'application/json',Authorization = apiKey))
+    datasource<-httr::content(req, as="text", encoding = "UTF-8")%>%fromJSON(flatten=TRUE,simplifyDataFrame = TRUE)
+  }  
+  fname=basename(endpoint)
+  csvFileName<-paste("out/tables/",fname,"_by_SID",".csv",sep = "")
+  write.csv(datasource,file=csvFileName,row.names = FALSE)
+}
+
+# define function for main report: Datausage                          
+get_report_datausage<-function(endpoint){
+  req<- httr::GET(url,path=endpoint,query=list(groupBy="none",from=dateFrom,to=dateTo) ,httr::add_headers(Accept = 'application/json',Authorization = apiKey))
+  datasource<-httr::content(req, as="text", encoding = "UTF-8")%>%fromJSON(flatten=TRUE,simplifyDataFrame = TRUE)
+  datasource_l<-lapply(datasource, flatten)
+  datasource<-do.call(data.frame,datasource_l)
+  rm(datasource_l)
+  SegmentCols<-(datasource[,grepl("^segmentIds",colnames(datasource))])
+      if (filterUI=="category"){
+      sid<-as.numeric(get_Id_list("out/tables/segments_by_CID.csv"))
+      datasource<-datasource[apply(SegmentCols [,],1,function(x) any(x %in% sid)),]
+      }
+      else if (filterUI=="segment/audience" & !(id=="(All)") & textstr=="(All)"){
+      sid<-id
+      datasource<-datasource[apply(SegmentCols [,],1,function(x) any(x %in% sid)),]
+      }
+      else if (filterUI=="segment/audience" & !(id=="(All)") & !(textstr=="(All)")){
+      sid<-id
+      datasource<-datasource[apply(SegmentCols [,],1,function(x) any(x %in% sid)),]
+      }
+      else if (filterUI=="segment/audience" & id=="(All)" & !(textstr=="(All)")) {
+      sid<-get_SID_list("out/tables/segments_by_PID.csv")%>%filter(str_detect(Audience.Name, fixed(textstr,ignore_case=TRUE)))%>%select(1)
+      sid<-as.numeric(as.character(sid$Audience.ID)) 
+      datasource<-datasource[apply(SegmentCols [,],1,function(x) any(x %in% sid)),]
+      }
+      else {datasource}
+  
+  fname=basename(endpoint)
+  csvFileName<-paste("out/tables/",fname,".csv",sep = "")
+  write.csv(datasource,file=csvFileName,row.names = FALSE)
+  # write table metadata - set new primary key
+  # app$writeTableManifest(csvFileName,destination='' ,primaryKey =c('Date'))
+}
+
+#======================DATA LOAD================================================#
+
+# 1. data used as input/filter for other reports
+
+## get list of dataProviders Is  
+get_report(endpoint="/v1/dmp/dataproviders")
+pid<-get_Id_list( "out/tables/dataproviders.csv")
+
+get_report(endpoint="/v1/dmp/agencies")
+
+## use dataProvidersId as selection
+get_report_pId(endpoint="/v1/dmp/dataproviders/{dataProviderId}/advertisers",pid)
+
+
+get_report_pId(endpoint="/v1/dmp/dataproviders/{dataProviderId}/categories",pid, filterType="category")
+get_report_pId(endpoint="/v1/dmp/dataProviders/{dataProviderId}/dataconsumers",pid, filterType="data consumers")
+get_report_pId(endpoint="/v1/dmp/dataproviders/{dataProviderId}/segments",pid,filterType=FALSE)
+
+
+cid<-get_Id_list( "out/tables/categories_by_PID.csv")
+dcid<-get_Id_list( "out/tables/dataconsumers_by_PID.csv")
+
+
+# 2. Overall reports - to be filtred based on user selection
+
+## get supplementary tables
+get_report_cId(endpoint="/v1/dmp/categories/{categoryId}/segments",cid)
+
+## get main table - DATAUSAGE
+get_report_datausage(endpoint="/v2/dmp/reports/datausage")  
+
+## get other supplementary tables
+get_report(endpoint="/v2/dmp/reports/billing/overall")
+get_report_audience(endpoint="/v2/dmp/reports/audience",pid)
+get_report_dcId(endpoint="/v1/dmp/dataconsumers/{dataConsumerId}/segments",dcid)
+
+# run only if filterUI="segment/audience" or ""
+# get_report_SId(endpoint="/v2/dmp/segments/{segmentId}/audience",sid)
+get_report_SId(endpoint="/v2/dmp/segments/{segmentId}/audience/comparison",sid)
+get_report_SId(endpoint="/v1/dmp/segments/{segmentId}/audience/dynamics",sid)
+get_report_SId(endpoint="/v1/dmp/segments/{segmentId}/audience/totals",sid)
 
 
 
