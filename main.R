4919a9f08f7eda284eb3cdb368bbda520dff9ce5
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2018-02-25 21:02:30 +0100

    Update main.R

--- main.R
+++ main.R
@@ -122,7 +122,7 @@ get_report_audience<-function(pid,endpoint){
       else if (filterUI=="segment/audience" & id=="(All)" & !(textstr=="(All)")) {
           sid<-get_SID_list("/data/out/tables/segments_by_PID.csv")%>%filter(str_detect(Audience_Name, fixed(textstr,ignore_case=TRUE)))%>%select(1)
           sid<-as.numeric(as.character(sid)) 
-          df <- df[df$Audience_ID %in% sid]
+          df <- df[df$Audience_ID %in% sid,]
           }
       else {df}
       
