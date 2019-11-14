9230919df0cb74b1d35b97e109b63b180a9978b1
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2019-11-14 10:03:33 +0100

    Update init.R

--- init.R
+++ init.R
@@ -1,8 +1,6 @@
 library('devtools')
 
 # install the transformation application ancestors
-devtools::install_version("curl", version = "4.1", repos = "http://cran.us.r-project.org",quiet = TRUE)
-devtools::install_version("httr", version = "1.4.0", repos = "http://cran.us.r-project.org",quiet = TRUE)
 devtools::install_github('keboola/r-application', ref = "master", force = TRUE)
 devtools::install_github('keboola/r-docker-application', ref = "master", force = TRUE)
 devtools::install_github("keboola/sapi-r-client", ref = "master", force = TRUE)
